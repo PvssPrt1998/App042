9e6c830bcfd6db51dae3aa0143ab6966d910f740
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2025-02-04 07:29:25 +0800

    Initial Commit

---
+++ App042/App042App.swift
@@ -0,0 +1,17 @@
+//
+//  App042App.swift
+//  App042
+//
+//  Created by Николай Щербаков on 04.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct App042App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
